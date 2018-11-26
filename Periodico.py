de486aba1de07ed02f6f1f664bd506d6a056c978
Kaio Rosa <kaiosrosa@gmail.com>
2018-11-26 18:08:32 -0200

    add Conferencia and Periodico (incomplete)

---
+++ Periodico.py
@@ -0,0 +1,10 @@
+class Periodico(object):
+
+    def __init__(self,ano, nome, veiculo, titulo, autores, numero, pg_inicial, pg_final):
+        self.nome = nome
+        self.veiculo = veiculo
+        self.titulo = titulo
+        self.autores = autores
+        self.numero = numero
+        self.pg_inicial = pg_inicial
+        self.pg_final = pg_final
